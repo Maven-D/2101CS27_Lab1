f58cb036ff8a6546e7b6fcf961cbecbe58b1f870
Divit <divitajmera15@gmail.com>
2022-07-27 10:33:38 +0530

    added selection sort

--- sorting.c
+++ sorting.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int length(int arr[]) {
+  return sizeof(arr) / sizeof(int);
+}
+
 void insertionSort(int arr[]) {
-  int len = sizeof(arr) / sizeof(int);
+  int len = length(arr);
   for (int i = 0; i < len; i++) {
       val = arr[i];
       int j = i - 1;
@@ -14,6 +18,26 @@ void insertionSort(int arr[]) {
 
       arr[j + 1] = val;
     }
+}
+
+void swap (int *num1, int *num2) {
+  *num1 ^= *num2;
+  *num2 ^= *num1;
+  *num1 ^= *num2;
+}
+
+void selectionSort(int arr[]) {
+  int len = length(arr);
+  for (int i = 0; i < len - 1; i++) {
+    int min_index = i;
+
+    // looping in the unsorted subarray for minimum element
+    for (int j = i + 1; j < len; j++)
+      if (arr[j] < arr[min_index])
+        min_index = j;
+
+    swap(&arr[i], &arr[min_index]);
+  }
 }
 
 
